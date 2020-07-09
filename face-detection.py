a4b78fa1a909cd737d05450f5703bdcfa7fb89fe
nadjet <gn_fekir@esi.dz>
2020-07-09 17:28:52 +0100

    face detection

---
+++ face-detection.py
@@ -0,0 +1,12 @@
+# viola and jones methods
+import cv2
+
+faceCascade = cv2.CascadeClassifier("Resources/haarcascade_frontalface_default.xml")
+img = cv2.imread("Resources/test5.jpg")
+imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = faceCascade.detectMultiScale(imgGrey, 1.1, 4)
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+cv2.imshow("Output", img)
+cv2.waitKey(0)
+
